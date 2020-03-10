b89ac1fd31613038ccbe7ce53747d439ac0bf14c
trang6789 <thuytrang16071989@gmail.com>
2020-03-10 20:49:32 +0700

    second practice for selenium

--- Selenium/Email.cs
+++ Selenium/Email.cs
@@ -29,16 +29,19 @@ namespace Selenium
             IWebElement open = driver.FindElement(By.XPath("//*[@class = 'gb_g' and @href ='https://mail.google.com/mail/?tab=wm&ogbl']"));
             open.Click();
             Thread.Sleep(2000);
-
+            driver.SwitchTo().Window(driver.WindowHandles.Last());
             IWebElement login = driver.FindElement(By.XPath("//*[@class = 'h-c-header__nav-li g-mail-nav-links']//*[@class = 'h-c-header__nav-li-link ' and @ga-event-action = 'sign in' ]"));
             login.Click();
             Thread.Sleep(2000);
 
             driver.SwitchTo();
             driver.Navigate().GoToUrl("https://accounts.google.com/signin/v2/identifier?continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&service=mail&sacu=1&rip=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin");
-            
-            IWebElement username = driver.FindElement(By.XPath("//*[@type = 'email' and @name = 'identifier' ]"));
+            //IWebElement username = driver.FindElement(By.XPath("//*[@type = 'email' and @name = 'identifier' ]"));
+            driver.SwitchTo().Window(driver.WindowHandles.Last());
+            IWebElement username = driver.FindElement(By.XPath("//*[@class = 'xkfVF']//*[@type = 'email' and @name = 'identifier' ]"));
             //IWebElement username = driver.FindElement(By.XPath("//input[@type= 'email']"));
+            Thread.Sleep(2000);
+            username.Click();
             username.SendKeys("Trang.test.mantu@gmail.com");
             IWebElement nextbutton = driver.FindElement(By.XPath("//*[@class= 'RveJvd snByac' and text() = 'Tiếp theo']"));
             nextbutton.Click();
@@ -57,26 +60,28 @@ namespace Selenium
         {
             driver.Navigate().GoToUrl("http://www.google.com/");
             driver.Manage().Window.Maximize();
-            IWebElement open = driver.FindElement(By.XPath("//*[@class = 'gb_g' and text()='Gmail']//self::a"));
+            IWebElement open = driver.FindElement(By.XPath("//*[@id = 'viewport']//descendant::a[text()='Gmail']"));
+            //IWebElement open = driver.FindElement(By.XPath("//*[@class = 'gb_g' and text()='Gmail']//self::a"));
             open.Click();
             Thread.Sleep(2000);
 
-            IWebElement login = driver.FindElement(By.XPath("//*[@class = 'h-c-header__cta-list header__nav--ltr' ]//descendant::a[2]"));
+            IWebElement login = driver.FindElement(By.XPath("//*[@class = 'h-c-header__cta-list header__nav--ltr' ]//descendant::a[contains( text(),'Đăng nhập')]"));
             login.Click();
             Thread.Sleep(2000);
 
             driver.SwitchTo();
             driver.Navigate().GoToUrl("https://accounts.google.com/signin/v2/identifier?continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&service=mail&sacu=1&rip=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin");
-            IWebElement username = driver.FindElement(By.XPath("//*[@type= 'email']//self::input"));
-            //IWebElement username = driver.FindElement(By.XPath("//input[@type= 'email']"));
+            driver.SwitchTo().Window(driver.WindowHandles.Last());
+            IWebElement username = driver.FindElement(By.XPath("//*[@type= 'email']"));
+            //IWebElement username = driver.FindElement(By.XPath("//input[@type= 'email']"));          
             username.SendKeys("Trang.test.mantu@gmail.com");
-            IWebElement nextbutton = driver.FindElement(By.XPath("//*[@id= 'identifierNext']//descendant::span[2]"));
+            IWebElement nextbutton = driver.FindElement(By.XPath("//*[@id= 'identifierNext']//descendant::span[contains( text(),'Tiếp theo')]"));
             nextbutton.Click();
             Thread.Sleep(2000);
 
-            IWebElement pass = driver.FindElement(By.XPath("//*[@type= 'password']//self::input"));
+            IWebElement pass = driver.FindElement(By.XPath("//*[@type= 'password']"));
             pass.SendKeys("mantu123456");
-            IWebElement next2 = driver.FindElement(By.XPath("//*[@id= 'passwordNext']//descendant::span[2]"));
+            IWebElement next2 = driver.FindElement(By.XPath("//*[@id= 'passwordNext']//descendant::span[contains( text(),'Tiếp theo')]"));
             next2.Click();
             Thread.Sleep(2000);
 
---
+++ Selenium/POM/LoginPageContains.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selenium.POM
+{
+    class LoginPageContains
+    {
+        public const string username = " Trang.test.mantu@gmail.com";
+        public const string pass = "mantu123456";
+        public const string baselink = "http://www.google.com/";
+        public const string openlink = "//*[@class = 'gb_g' and @href ='https://mail.google.com/mail/?tab=wm&ogbl']";
+        public const string login = "//*[@class = 'h-c-header__nav-li g-mail-nav-links']//*[@class = 'h-c-header__nav-li-link ' and @ga-event-action = 'sign in' ]";
+        public const string loginlink = "https://accounts.google.com/signin/v2/identifier?continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&service=mail&sacu=1&rip=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin";
+    }
+}
