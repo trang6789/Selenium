873b7618a32652eb587ab525586d95cbcc925707
trang6789 <thuytrang16071989@gmail.com>
2020-03-13 17:59:28 +0700

    Four practice for selenium

--- Selenium/POM/LoginPageContains.cs
+++ Selenium/POM/LoginPageContains.cs
@@ -8,15 +8,7 @@ namespace Selenium.POM
 {
     class LoginPageContains
     {
-        public const string baselink = "http://www.google.com/";
-        public const string openlink = "//*[@id = 'viewport']//descendant::a[text()='Gmail']";
-        public const string login = "//*[@class = 'h-c-header__cta-list header__nav--ltr' ]//descendant::a[contains( text(),'Đăng nhập')]";
-        public const string loginlink = "https://accounts.google.com/signin/v2/identifier?continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&service=mail&sacu=1&rip=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin";
         public const string username = "Trang.test.mantu@gmail.com";
         public const string pass = "mantu123456";
-        public const string usernamelink = "//*[@type= 'email']";
-        public const string passlink = "//*[@type= 'password']";
-        public const string nextbutton1 = "//*[@id= 'identifierNext']//descendant::span[contains( text(),'Tiếp theo')]";
-        public const string nextbutton2 = "//*[@id= 'passwordNext']//descendant::span[contains( text(),'Tiếp theo')]";
     }
 }
--- Selenium/Testscript/TestLogin.cs
+++ Selenium/Testscript/TestLogin.cs
@@ -30,30 +30,34 @@ namespace Selenium.Testscript
         [Test]
         public void testLogin()
         {
-            driver.Navigate().GoToUrl(LoginPageContains.baselink);
-            driver.Manage().Window.Maximize();
+            //driver.Navigate().GoToUrl(LoginPageLocator.baselink);
+            //driver.Manage().Window.Maximize();
+            LoginPageHelper.goToPage(LoginPageLocator.baselink, driver);
 
-            IWebElement openlink = driver.FindElement(By.XPath(LoginPageContains.openlink));
+            IWebElement openlink = driver.FindElement(By.XPath(LoginPageLocator.openlink));
             openlink.Click();
             Thread.Sleep(2000);
                        
-            IWebElement login = driver.FindElement(By.XPath(LoginPageContains.login));
+            IWebElement login = driver.FindElement(By.XPath(LoginPageLocator.login));
             login.Click();
             Thread.Sleep(2000);
 
-            driver.Navigate().GoToUrl(LoginPageContains.loginlink);
+            //driver.Navigate().GoToUrl(LoginPageLocator.loginlink);
             driver.SwitchTo().Window(driver.WindowHandles.Last());
-            IWebElement username = driver.FindElement(By.XPath(LoginPageContains.usernamelink));
+           
+            IWebElement username = driver.FindElement(By.XPath(LoginPageLocator.usernamelink));
             LoginPageHelper.inputUserName(LoginPageContains.username, username);
-            IWebElement nextbutton1 = driver.FindElement(By.XPath(LoginPageContains.nextbutton1));
+            IWebElement nextbutton1 = driver.FindElement(By.XPath(LoginPageLocator.nextbutton1));
             nextbutton1.Click();
             Thread.Sleep(2000);
 
-            IWebElement pass = driver.FindElement(By.XPath(LoginPageContains.passlink));
+            IWebElement pass = driver.FindElement(By.XPath(LoginPageLocator.passlink));
             LoginPageHelper.inputPassWord(LoginPageContains.pass, pass);
             Thread.Sleep(2000);
-            IWebElement nextbutton2 = driver.FindElement(By.XPath(LoginPageContains.nextbutton2));
+            IWebElement nextbutton2 = driver.FindElement(By.XPath(LoginPageLocator.nextbutton2));
             nextbutton2.Click();
+            Thread.Sleep(10000);
+            
 
         }
     }
